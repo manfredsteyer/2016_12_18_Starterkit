047ab82eec3c614152c209c716777df428c218e0
ManfredSteyer <manfred.steyer@gmx.net>
2017-02-20 22:10:47 +0100

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
-
-import { AppState } from './app.service';
+import { CityPipe } from './shared/pipes/city.pipe';
 
 @Component({
   selector: 'app',
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,35 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { FlightSearchComponent } from './flight-search/flight-search.component';
+import { FlightService } from './flight-search/flight.service';
+import { BASE_URL } from './app.tokens';
+import { CityPipe } from './shared/pipes/city.pipe';
+import { FlightCardComponent } from './flight-search/flight-card.component';
+import { HomeComponent } from './home/home.component';
+import { PassengerSearchComponent } from './passenger-search/passenger-search.component';
+import { AppRouterModule } from './app.routes';
+import { FlightEditComponent } from './flight-edit/flight-edit.component';
 
 @NgModule({
   imports: [ 
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    AppRouterModule
   ],
   declarations: [
-    AppComponent
+    AppComponent,
+    FlightSearchComponent,
+    CityPipe,
+    FlightCardComponent,
+    HomeComponent,
+    PassengerSearchComponent,
+    FlightEditComponent
+  ],
+  providers: [
+    { provide: BASE_URL, useValue: 'http://www.angular.at/api' },
+    FlightService
   ],
   bootstrap: [        
      AppComponent 
